fbf7949a935458f937a5eaaf73a0d9c25bbfe0ea
RiyaRani23 <riyarani1404@gmail.com>
2025-10-09 09:54:50 +0530

    Reading and writing File Asynchronously

--- 1-NODE-FARM/index.js
+++ 1-NODE-FARM/index.js
@@ -1,14 +1,30 @@
 // READING AND WRITING FILES 
-
+// utf-8 encoding is important to get a string(english) instead of a buffer 
 
 const fs = require('fs');
 
-// utf-8 encoding is important to get a string(english) instead of a buffer 
-const textIn = fs.readFileSync('starter/txt/input.txt', 'utf-8');
-console.log(textIn);
+// Blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('starter/txt/output.txt', textOut);
-console.log('File written!');
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written!');
+ 
 
-// READING AND WRITING FILES 
+// Non-blocking, asynchronous way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+    if (err) return console.log('Error! 💥');
+   
+    fs.readFile(`./txt/${data}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2); 
+        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+            console.log(data3);
+
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('Your file has been written 😁');
+            });
+        });
+    });
+});
+console.log('Will read file!');
