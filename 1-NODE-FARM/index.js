1570211c73782eafe0ef69c2fa0d4bb6e3b7f8f9
RiyaRani23 <riyarani1404@gmail.com>
2025-10-09 12:36:13 +0530

    Router

--- 1-NODE-FARM/index.js
+++ 1-NODE-FARM/index.js
@@ -3,6 +3,7 @@
 
 const fs = require('fs');
 const https = require('https');
+const url = require('url');
 
 // ********************** FILES **********************
 
@@ -36,9 +37,24 @@ const https = require('https');
 // ********************** SERVER **********************
 
 const server = https.createServer((req, res) => {
-    console.log(req);
-    res.end('Hello from the server!'); 
-});
+    const pathName = req.url;
+
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW');
+    }
+    else if (pathName === '/product') {
+        res.end('This is the PRODUCT');
+    } 
+    else {
+        // res.end('Page not found');
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
+        });
+   
 
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to requests on port 8000');
